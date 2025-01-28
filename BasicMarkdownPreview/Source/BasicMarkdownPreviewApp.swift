3ee435a187e85dbf1800af6d594d77b9e4c8727d
Colbyn Wadman <hello@colbyn.com>
2025-01-28 12:44:43 -0700

    initialize workspace setup

---
+++ BasicMarkdownPreview/Source/BasicMarkdownPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicMarkdownPreviewApp.swift
+//  BasicMarkdownPreview
+//
+//  Created by Colbyn Wadman on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct BasicMarkdownPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("TODO")
+        }
+    }
+}
