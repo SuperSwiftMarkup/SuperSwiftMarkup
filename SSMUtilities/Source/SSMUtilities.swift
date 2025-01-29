005cf746e28033c68ef80357aae28a08a8e5bbc7
Colbyn Wadman <hello@colbyn.com>
2025-01-28 18:17:46 -0700

    update source code headers

--- SSMUtilities/Source/SSMUtilities.swift
+++ SSMUtilities/Source/SSMUtilities.swift
@@ -1,2 +1,10 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+// Created by Colbyn Wadman on 2025-1-28 (ISO 8601)
+//
+// All SuperSwiftMarkup source code and other software material is available
+// under a dual licensing model. Users may choose to use such under either:
+// 1. The GNU Affero General Public License v3.0 ("AGPLv3"); or
+// 2. A commercial license, as specified in LICENSE file.
+//
+// https://github.com/SuperSwiftMarkup/SuperSwiftMarkup/blob/main/LICENSE.md
+//
